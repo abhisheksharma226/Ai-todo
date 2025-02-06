9b8a3f1e0cbc3c6b4e063a7cb7e8f51de4fca845
abhisheksharma226 <avisheksharma2004@gmail.com>
2025-02-06 23:27:40 +0530

    429 error

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import dotenv from "dotenv";
 import { ilike, eq } from 'drizzle-orm';
 import {db} from './db/index.js';
 import { todosTable } from './db/schema.js';
@@ -5,8 +6,12 @@ import OpenAI from "openai";
 import readlineSync from 'readline-sync';
 import { type } from 'os';
 
-const client = new OpenAI();
 
+dotenv.config();
+
+const client = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
 
 async function getAllTodos() {
     const todo = await db.select(todosTable).from(todosTable);
@@ -30,7 +35,7 @@ async function searchTodo(search) {
     const todo = await db
     .select()
     .from(todosTable)
-    .where(ilike(todosTable.todo, search));
+    .where(ilike(todosTable.todo, `%${search}%`));
     return todo;
 }
 
@@ -44,10 +49,10 @@ const tools = {
     getAllTodos : getAllTodos,
     createTodo : createTodo,
     searchTodo : searchTodo,
-    deleteTodoById : deleteTodoById
+    deleteTodoById : deleteTodoById,
 }
 
-const SYTSTEM_PROMPT = `
+const SYSTEM_PROMPT = `
 You are an AI To-Do List Assistant with START, PLAN, ACTION, Obeservation and Output State.
 Wait for the user prompt and first PLAN using available tools.
 After Planning, Take the action with appropriate tools and wait for Observation based on Action.
@@ -77,37 +82,37 @@ START
 { "type": "output", "output": "Can you tell me what all items you want to shop for?" }
 { "type": "user", "user": "I want to shop for milk, kurkure, lays and choco." }
 { "type": "plan", "plan": "I will use createTodo to create a new Todo in DB."}
-{ "type": "laction", "function": "createTodo", "input": "Shopping for milk, kurkure, lays and choco."
-{ "type": "observation", "observation": "2")
+{ "type": "laction", "function": "createTodo", "input": "Shopping for milk, kurkure, lays and choco."}
+{ "type": "observation", "observation": "2"}
 { "type": "output", "output": "You todo has been added successfully" }
 `;
 
 
-const message = [{ role: "system", content: SYTSTEM_PROMPT }]; 
+const messages = [{ role: "system", content: SYSTEM_PROMPT }]; 
 
 
 while (true) {
     const query = readlineSync.question(">> ");
     const userMessage = { 
-        type: "user", 
+        type: 'user', 
         user: query 
     };
 
-    message.push({ role: "user", content: JSON.stringify(userMessage) });
+    messages.push({ role: 'user', content: JSON.stringify(userMessage) });
 
 
     while (true) {
         const chat = await client.chat.completions.create({
             model: 'gpt-3.5-turbo',
-            messages: message,
+            messages: messages,
             response_format: { type: 'json_object' },
         }); 
-            const result = chat.choices [0].message.content;
+            const result = chat.choices[0].message.content;
             messages.push({ role: 'assistant', content: result });
 
             const action = JSON.parse(result);
             if (action.type === 'output'){
-                console.log(': ${action.output}');
+                console.log(`: ${action.output}`);
                 break;
             }else if(action.type === 'action'){
                 const fn = tools[action.function];
